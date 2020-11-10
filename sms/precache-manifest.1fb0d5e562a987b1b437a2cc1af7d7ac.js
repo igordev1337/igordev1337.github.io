0635d8ea7a43b2f6918a39800048ba9f95a2e32b
igordev1337 <68248891+igordev1337@users.noreply.github.com>
2020-11-10 19:54:33 +0300

    Add files via upload

---
+++ sms/precache-manifest.1fb0d5e562a987b1b437a2cc1af7d7ac.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fc131c8a66431ce3f55dbacc32e6f391",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3cee094f7e949548cb6b",
+    "url": "/static/css/2.6326594c.chunk.css"
+  },
+  {
+    "revision": "3cee094f7e949548cb6b",
+    "url": "/static/js/2.bbf7580a.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/static/js/2.bbf7580a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "417701fb42e20300764f",
+    "url": "/static/js/main.f8ee92ba.chunk.js"
+  },
+  {
+    "revision": "041542dab35f444ffb8e",
+    "url": "/static/js/runtime-main.ac56c7b9.js"
+  }
+]);
